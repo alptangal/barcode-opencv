688de1a6c39e04c50440c9da8460e852d41c86c5
alptangal <alptangal@gmail.com>
2024-07-13 19:15:41 +0700

    .

--- main.py
+++ main.py
@@ -66,6 +66,7 @@ def scan_qr_code():
         qr_codes=decode_qr_code(img)
         print(qr_codes)
         st.write("QR Code Scanner123:"+str(qr_codes) if qr_codes else '')
+    st.write('hello world')
     if st.session_state.scanning:
         cap = cv2.VideoCapture(-1)
         print(cap)
