b9cd89d903e698307cf7599bee962511dc50897e
phuongDoMega <pdmega268@gmail.com>
2024-07-13 23:37:33 +0700

    .

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ from PIL import Image
 import cv2
 from pyzbar import pyzbar
 from pyzbar.pyzbar import ZBarSymbol
+from streamlit_webrtc import webrtc_streamer
 
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:]
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
 def decode_qr_code(frame):
     """
     Decodes QR codes in the given image frame.
