def111a9922174cf4f70abe4fdc768f3ed5fcd8a
alptangal <alptangal@gmail.com>
2024-07-14 18:56:12 +0700

    .

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ result_queue: "queue.Queue[List[Detection]]" = queue.Queue()
 
 rs=[]
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    rs=[]
     st.title('hello')
     st.write('hello world')
     image = frame.to_ndarray(format="bgr24")
