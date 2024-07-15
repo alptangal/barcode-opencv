d0956b7ad1aeda064178e44a19c365baa245c0b4
alptangal <alptangal@gmail.com>
2024-07-15 10:07:57 +0700

    .

--- sample_utils/turn.py
+++ sample_utils/turn.py
@@ -9,5 +9,5 @@ logger = logging.getLogger(__name__)
 
 
 def get_ice_servers():
-    return [ { 'urls': 'stun:freeturn.net:3479' }, { 'urls': 'turn:freeturn.net:3479', 'username': 'free', 'credential': 'free' } ]
+    #return [ { 'urls': 'stun:freeturn.net:3479' }, { 'urls': 'turn:freeturn.net:3479', 'username': 'free', 'credential': 'free' } ]
     return [{"urls":"stun:stun.relay.metered.ca:80"},{"urls":"turn:global.relay.metered.ca:80","username":"78f36f157b772e52ce9d6a43","credential":"ubUcdj11HN7FSrC2"},{"urls":"turn:global.relay.metered.ca:80?transport=tcp","username":"78f36f157b772e52ce9d6a43","credential":"ubUcdj11HN7FSrC2"},{"urls":"turn:global.relay.metered.ca:443","username":"78f36f157b772e52ce9d6a43","credential":"ubUcdj11HN7FSrC2"},{"urls":"turns:global.relay.metered.ca:443?transport=tcp","username":"78f36f157b772e52ce9d6a43","credential":"ubUcdj11HN7FSrC2"}]
